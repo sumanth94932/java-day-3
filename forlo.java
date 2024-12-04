5373fc23803cf70e2f388d977c28ddf47b3ffffe
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-04 10:18:31 +0530

    Add files via upload

---
+++ forlo.java
@@ -0,0 +1,14 @@
+
+ class forlo{
+    public static void main(String args[]){
+       int a=6;
+        if((a&1)==0)
+        {
+            System.out.println("it is even");
+        }
+        else 
+        {
+        System.out.println("it is odd");
+        }
+    }
+}
