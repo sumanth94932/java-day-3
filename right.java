8daf9fb8835962165f6d80bf4c4db78c7ebdc79f
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-04 16:09:35 +0530

    Add files via upload

---
+++ right.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class right {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //local variable for the class and global variable main method
+        int a=sc.nextInt();
+        int p=sc.nextInt();
+        System.out.println(a>>p);
+    }
+}
