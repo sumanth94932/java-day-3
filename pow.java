20de08346739a97503acfebbbd9a26f73088287d
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-04 11:10:46 +0530

    Add files via upload

---
+++ pow.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class pow{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        System.out.println("power of a is :"+(1<<a));
+
+    }
+}
