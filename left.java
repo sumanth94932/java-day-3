0be3430768d900f8283591cc236702d768e2c228
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-04 16:09:59 +0530

    Add files via upload

---
+++ left.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class left {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //local variable for the class and global variable main method
+        int a=sc.nextInt();
+        int p=sc.nextInt();
+        System.out.println(a<<p);
+    }
+}
